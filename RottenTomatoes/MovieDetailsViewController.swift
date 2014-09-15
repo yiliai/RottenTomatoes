6cac5ad545c9040c26f60d4dde9f96aaa0398a0a
Yili Aiwazian <yili@grandtrunklabs.com>
2014-09-14 23:35:33 -0700

    Cleaned up most of the magic numbers

--- RottenTomatoes/MovieDetailsViewController.swift
+++ RottenTomatoes/MovieDetailsViewController.swift
@@ -30,84 +30,72 @@ class MovieDetailsViewController: UIViewController {
         //self.navigationController?.navigationBar.translucent = true
         
         scrollView.scrollEnabled = true
-        scrollView.contentSize = CGSize(width: 320,height: 1000)
+        scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height)
         
         movieCardView.backgroundColor = UIColor(white: 0.0, alpha: 0.8)
-        movieCardView.frame = CGRectMake(0,GAP_FROM_TOP,320,1000)
+        movieCardView.frame = CGRectMake(0,GAP_FROM_TOP,self.view.frame.width,self.view.frame.height)
+        
+        // Set movie poster as the background
+        moviePosterImage.frame = CGRectMake(0,0,self.view.frame.width,self.view.frame.height)
+        if let moviePosters = movieDictionary["posters"] as? NSDictionary {
+            
+            if let small_thumbnailURL = moviePosters["thumbnail"] as? String {
+                println(small_thumbnailURL)
                 
-        let moviePosters = movieDictionary["posters"] as NSDictionary
-        let small_thumbnailURL = moviePosters["thumbnail"] as String
-    
-        let thumbnailURL = small_thumbnailURL.stringByReplacingOccurrencesOfString("_tmb.jpg", withString: "_ori.jpg", options: NSStringCompareOptions.LiteralSearch, range: nil)
-        
-        let request = NSURLRequest(URL: NSURL.URLWithString(small_thumbnailURL as NSString))
+                let thumbnailURL = small_thumbnailURL.stringByReplacingOccurrencesOfString("_tmb.jpg", withString: "_ori.jpg", options: NSStringCompareOptions.LiteralSearch, range: nil)
+                let request = NSURLRequest(URL: NSURL.URLWithString(small_thumbnailURL as NSString))
+                
+                moviePosterImage.alpha = 0.0
+                moviePosterImage.setImageWithURLRequest(request, placeholderImage: nil, success: { (request, response, image) -> Void in
+                    UIView.animateWithDuration(0.4, animations: { () -> Void in
+                        self.moviePosterImage.image = image
+                        self.moviePosterImage.alpha = 1.0
+                    })
+                    self.moviePosterImage.setImageWithURL(NSURL.URLWithString(thumbnailURL))
+                    }, failure: nil)
+            }
+        }
 
-        moviePosterImage.alpha = 0.0
-        moviePosterImage.setImageWithURLRequest(request, placeholderImage: nil, success: { (request, response, image) -> Void in
-            UIView.animateWithDuration(0.4, animations: { () -> Void in
-                self.moviePosterImage.image = image
-                self.moviePosterImage.alpha = 1.0
-            })
-            self.moviePosterImage.setImageWithURL(NSURL.URLWithString(thumbnailURL))
-        }, failure: nil)
+        var h_offset = PADDING
         
-
-        moviePosterImage.frame = CGRectMake(0,0,320,568)
-        
-        var h_offset = CGFloat(10.0)
-        
-        let movieTitle = movieDictionary["title"] as NSString
-        let movieYear = movieDictionary["year"] as NSInteger
-        let synopsis = movieDictionary["synopsis"] as NSString
-        
-        let ratings = movieDictionary["ratings"] as NSDictionary
-        let critics_score = String(ratings["critics_score"] as NSInteger)
-        let critics_rating = ratings["critics_rating"] as NSString
-        let audience_score = String(ratings["audience_score"] as NSInteger)
-        let audience_rating = ratings["audience_rating"] as NSString
-        let rating = "Critics Rating: " + critics_score + "% " + critics_rating + "\nAudience Rating: " + audience_score + "% " + audience_rating
-        
-        movieNameLabel.text = "\(movieTitle)" + " (" + "\(String(movieYear))" + ")"
-        
-        movieNameLabel.frame = CGRectMake(10,h_offset,movieCardView.frame.width-20,50)
+        // Set movie title and year
+        movieNameLabel.text = ""
+        if let movieTitle = movieDictionary["title"] as? NSString {
+            movieNameLabel.text = String(movieTitle)
+        }
+        if let movieYear = movieDictionary["year"] as? NSInteger {
+            movieNameLabel.text = movieNameLabel.text! + " (" + "\(String(movieYear))" + ")"
+        }
+        movieNameLabel.frame = CGRectMake(PADDING,h_offset,movieCardView.frame.width-2*PADDING,0)
         movieNameLabel.numberOfLines = 0
         movieNameLabel.sizeToFit()
+        h_offset += movieNameLabel.frame.height + PADDING
 
-        h_offset += movieNameLabel.frame.height + 10
-
-        movieRatingLabel.text = rating
-        movieRatingLabel.frame = CGRectMake(10,h_offset,movieCardView.frame.width-20,500)
-        movieRatingLabel.numberOfLines = 0
-        movieRatingLabel.sizeToFit()
-
-        h_offset += movieRatingLabel.frame.height + 10
-
-        movieSynopsisLabel.text = synopsis
-        movieSynopsisLabel.frame = CGRectMake(10,h_offset,movieCardView.frame.width-20,500)
-        movieSynopsisLabel.numberOfLines = 0
-        movieSynopsisLabel.sizeToFit()
+        if let ratings = movieDictionary["ratings"] as? NSDictionary {
+            let critics_score = String(ratings["critics_score"] as NSInteger)
+            let critics_rating = ratings["critics_rating"] as NSString
+            let audience_score = String(ratings["audience_score"] as NSInteger)
+            let audience_rating = ratings["audience_rating"] as NSString
+            let rating = "Critics Rating: " + critics_score + "% " + critics_rating + "\nAudience Rating: " + audience_score + "% " + audience_rating
+            movieRatingLabel.text = rating
+            movieRatingLabel.frame = CGRectMake(PADDING,h_offset,movieCardView.frame.width-2*PADDING,0)
+            movieRatingLabel.numberOfLines = 0
+            movieRatingLabel.sizeToFit()
+            h_offset += movieRatingLabel.frame.height + PADDING
+        }
         
-        let movieCardHeight = max(movieSynopsisLabel.frame.origin.y + movieSynopsisLabel.frame.height + 10, self.view.frame.height - GAP_FROM_TOP)
+        if let synopsis = movieDictionary["synopsis"] as? NSString {
+            movieSynopsisLabel.text = synopsis
+            movieSynopsisLabel.frame = CGRectMake(PADDING,h_offset,movieCardView.frame.width-2*PADDING,0)
+            movieSynopsisLabel.numberOfLines = 0
+            movieSynopsisLabel.sizeToFit()
+        }
+        
+        let movieCardHeight = max(movieSynopsisLabel.frame.origin.y + movieSynopsisLabel.frame.height + PADDING, self.view.frame.height - GAP_FROM_TOP)
         
         movieCardView.frame.size = CGSize(width: movieCardView.frame.width, height: movieCardHeight)
         scrollView.contentSize = CGSize(width: movieCardView.frame.width, height: movieCardView.frame.height + GAP_FROM_TOP)
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- RottenTomatoes/MoviesCollectionViewController.swift
+++ RottenTomatoes/MoviesCollectionViewController.swift
@@ -13,6 +13,12 @@ let BG_GRAY = UIColor(white: 0.96, alpha: 1.0)
 let TOP_MOVIES_IN_THEATERS = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json"
 let TOP_DVD = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/current_releases.json"
 let PAGE_LIMIT = 10
+let PADDING = CGFloat(10)
+let MARGIN = CGFloat(8)
+let RATING_IMAGE_SIZE = CGFloat(15)
+let POSTER_ASPECT_RATIO = CGFloat(54.0/80.0)
+let NAV_BAR_HEIGHT = CGFloat(64)
+let SEARCH_BAR_HEIGHT = CGFloat(50)
 
 class MoviesCollectionViewController: UIViewController, UICollectionViewDataSource, UITextFieldDelegate {
 
@@ -90,6 +96,7 @@ class MoviesCollectionViewController: UIViewController, UICollectionViewDataSour
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("movieCollectionCell", forIndexPath: indexPath) as MovieCollectionViewCell
         
+        // Check to see if the user is searching to return the right item
         var movieDictionary :NSDictionary
         if (searchField.text == "") {
             movieDictionary = self.moviesArray![indexPath.row] as NSDictionary
@@ -98,49 +105,64 @@ class MoviesCollectionViewController: UIViewController, UICollectionViewDataSour
             movieDictionary = self.movieSearchResultsArray![indexPath.row] as NSDictionary
         }
         
-        cell.movieNameLabel.text = movieDictionary["title"] as NSString
-        cell.movieNameLabel.frame = CGRectMake(8,210,128,50)
-        cell.movieNameLabel.numberOfLines = 0
-        cell.movieNameLabel.sizeToFit()
-        let h_offset = cell.movieNameLabel.frame.origin.y + cell.movieNameLabel.frame.height + 5
+        // Set the movie title
+        var h_offset = CGFloat(cell.frame.width-2)/(POSTER_ASPECT_RATIO)
+        
+        if let movieTitle = movieDictionary["title"] as? NSString {
+            cell.movieNameLabel.text = movieTitle
+            cell.movieNameLabel.frame = CGRectMake(MARGIN,h_offset,cell.frame.width-2*MARGIN,0)
+            cell.movieNameLabel.numberOfLines = 0
+            cell.movieNameLabel.sizeToFit()
+        }
+        h_offset = cell.movieNameLabel.frame.origin.y + cell.movieNameLabel.frame.height + PADDING/2
+
+        // Set the MPAA rating
         if let mpaa_rating = movieDictionary["mpaa_rating"] as? NSString {
             cell.movieRatingLabel.text = " \(mpaa_rating) "
             cell.movieRatingLabel.textColor = LIGHT_GRAY
             cell.movieRatingLabel.layer.borderWidth = 1.0
             cell.movieRatingLabel.layer.borderColor = LIGHT_GRAY.CGColor
-            cell.movieRatingLabel.frame = CGRectMake(8,h_offset,128,40)
+            cell.movieRatingLabel.frame = CGRectMake(MARGIN,h_offset,0,0)
             cell.movieRatingLabel.sizeToFit()
         }
+        
+        // Set the runtime
         if let runtime = movieDictionary["runtime"] as? NSInteger {
-            let x_offset = cell.movieRatingLabel.frame.origin.x + cell.movieRatingLabel.frame.width + 10
+            let x_offset = cell.movieRatingLabel.frame.origin.x + cell.movieRatingLabel.frame.width + PADDING
             cell.movieRuntimeLabel.text = "\(runtime) min"
             cell.movieRuntimeLabel.textColor = LIGHT_GRAY
-            cell.movieRuntimeLabel.frame = CGRectMake(x_offset,h_offset,128,40)
+            cell.movieRuntimeLabel.frame = CGRectMake(x_offset,h_offset,0,0)
             cell.movieRuntimeLabel.sizeToFit()
         }
-        let ratings = movieDictionary["ratings"] as NSDictionary
-        let critics_score = ratings["critics_score"] as NSInteger
-        if (critics_score > 50) {
-            cell.ratingImage.image = UIImage(named: "rating_fresh")
+        
+        // Set the critics rating
+        if let ratings = movieDictionary["ratings"] as? NSDictionary {
+            if let critics_score = ratings["critics_score"] as? NSInteger {
+                if (critics_score > 50) {
+                    cell.ratingImage.image = UIImage(named: "rating_fresh")
+                }
+                else {
+                    cell.ratingImage.image = UIImage(named: "rating_rotten")
+                }
+                var x_offset = cell.movieRuntimeLabel.frame.origin.x + cell.movieRuntimeLabel.frame.width + PADDING
+                cell.ratingImage.frame = CGRectMake(x_offset,h_offset-2,RATING_IMAGE_SIZE,RATING_IMAGE_SIZE)
+                x_offset += cell.ratingImage.frame.width + 2
+            
+                cell.ratingLabel.text = "\(String(critics_score))%"
+                cell.ratingLabel.textColor = LIGHT_GRAY
+                cell.ratingLabel.frame = CGRectMake(x_offset,h_offset,0,0)
+                cell.ratingLabel.sizeToFit()
+            }
         }
-        else {
-            cell.ratingImage.image = UIImage(named: "rating_rotten")
+        
+        // Set the poster image
+        if let moviePosters = movieDictionary["posters"] as? NSDictionary {
+            if var thumbnailURL = moviePosters["thumbnail"] as? String {
+                thumbnailURL = thumbnailURL.stringByReplacingOccurrencesOfString("_tmb.jpg", withString: "_det.jpg", options: NSStringCompareOptions.LiteralSearch, range: nil)
+                
+                fadeInImageFromURL(cell.moviePosterImage, url: NSURL.URLWithString(thumbnailURL as NSString))
+            }
         }
-        var x_offset = cell.movieRuntimeLabel.frame.origin.x + cell.movieRuntimeLabel.frame.width + 10
-        cell.ratingImage.frame = CGRectMake(x_offset,h_offset-2,15,15)
-        x_offset += cell.ratingImage.frame.width + 2
-        
-        cell.ratingLabel.text = "\(String(critics_score))%"
-        cell.ratingLabel.textColor = LIGHT_GRAY
-        cell.ratingLabel.frame = CGRectMake(x_offset,h_offset,128,40)
-        cell.ratingLabel.sizeToFit()
-        
-        let moviePosters = movieDictionary["posters"] as NSDictionary
-        var thumbnailURL = moviePosters["thumbnail"] as String
-        thumbnailURL = thumbnailURL.stringByReplacingOccurrencesOfString("_tmb.jpg", withString: "_det.jpg", options: NSStringCompareOptions.LiteralSearch, range: nil)
-        
-        fadeInImageFromURL(cell.moviePosterImage, url: NSURL.URLWithString(thumbnailURL as NSString))
-        
         cell.contentView.layer.cornerRadius = 4.0
         cell.contentView.layer.masksToBounds = true;
         cell.contentView.backgroundColor = UIColor.whiteColor()
@@ -156,6 +178,7 @@ class MoviesCollectionViewController: UIViewController, UICollectionViewDataSour
             let detailsViewController: MovieDetailsViewController = segue.destinationViewController as MovieDetailsViewController
             let indexPath = self.moviesCollectionView.indexPathsForSelectedItems()[0] as NSIndexPath
             detailsViewController.movieDictionary = self.moviesArray![indexPath.row] as NSDictionary
+            detailsViewController.hidesBottomBarWhenPushed = true
         }
     }
     
@@ -165,6 +188,7 @@ class MoviesCollectionViewController: UIViewController, UICollectionViewDataSour
     
     func loadRottenTomatoesData(fetchMore :Bool = false) {
         let YourApiKey = "cvyj5jz6rkzkscxus99qwvay"
+        
         // Calculate the next page number
         var page = 1
         if (moviesArray != nil) {
@@ -184,7 +208,7 @@ class MoviesCollectionViewController: UIViewController, UICollectionViewDataSour
         progressControl.startAnimating()
         progressControl.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
         progressControl.color = UIColor.lightGrayColor()
-        progressControl.frame = CGRectMake(self.view.frame.width/2-10, self.view.frame.height/2-10, 20, 20)
+        progressControl.frame = CGRectMake(self.view.frame.width/2-PADDING, self.view.frame.height/2-PADDING, 2*PADDING, 2*PADDING)
         
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler:{ (response, data, error) in
             
@@ -215,7 +239,6 @@ class MoviesCollectionViewController: UIViewController, UICollectionViewDataSour
                     println(self.moviesArray?.count)
                 }
             }
-            
             self.refreshControl.endRefreshing()
             self.progressControl.hidden = true
             // Removing the progress control from the view so that when you pull to refresh, there aren't 2 spinners.
@@ -232,16 +255,16 @@ class MoviesCollectionViewController: UIViewController, UICollectionViewDataSour
         if (fullscreen) {
             errorLabel.textColor = UIColor.blackColor()
             errorLabel.textAlignment = NSTextAlignment.Center
-            errorLabel.frame = CGRectMake(50, self.view.frame.height/2, 220, 568)
+            errorLabel.frame = CGRectMake(PADDING*5, self.view.frame.height/2, self.view.frame.width-PADDING*10, self.view.frame.height)
             errorLabel.sizeToFit()
         }
         else {
-            errorLabel.frame = CGRectMake(10, 10, 300, 100)
+            errorLabel.frame = CGRectMake(PADDING, PADDING, self.view.frame.width-2*PADDING, 0)
             errorLabel.textColor = UIColor.whiteColor()
             errorLabel.sizeToFit()
             
             errorView.layer.backgroundColor = UIColor.darkGrayColor().CGColor
-            errorView.frame = CGRectMake(0, 64, self.view.frame.width, errorLabel.frame.height+20)
+            errorView.frame = CGRectMake(0, NAV_BAR_HEIGHT+SEARCH_BAR_HEIGHT, self.view.frame.width, errorLabel.frame.height+20)
             errorView.layer.shadowRadius = 5.0
             errorView.layer.shadowColor = UIColor.darkGrayColor().CGColor
             errorView.layer.shadowOpacity = 1.0
@@ -254,11 +277,8 @@ class MoviesCollectionViewController: UIViewController, UICollectionViewDataSour
     }
     
     func fadeInImageFromURL(imageView :UIImageView, url: NSURL) {
-        
         let request = NSURLRequest(URL: url)
-        
         imageView.setImageWithURLRequest(request, placeholderImage: nil, success: { (request, response, image) -> Void in
-            
             if (response == nil) {
                 imageView.image = image
                 return
@@ -272,14 +292,13 @@ class MoviesCollectionViewController: UIViewController, UICollectionViewDataSour
     }
     
     func showSearchField() {
-        
-        searchBarView.frame = CGRectMake(0, 64, self.view.frame.width, 50)
+        searchBarView.frame = CGRectMake(0, NAV_BAR_HEIGHT, self.view.frame.width, SEARCH_BAR_HEIGHT)
         searchBarView.layer.backgroundColor = BG_GRAY.CGColor
         searchBarView.layer.shadowRadius = 5.0
         searchBarView.layer.shadowColor = UIColor.darkGrayColor().CGColor
         searchBarView.layer.shadowOpacity = 1.0
         
-        searchField.frame = CGRectMake(10, 10, self.view.frame.width-20, 30)
+        searchField.frame = CGRectMake(PADDING, PADDING, self.view.frame.width-2*PADDING, SEARCH_BAR_HEIGHT-2*PADDING)
         searchField.borderStyle = UITextBorderStyle.RoundedRect
         searchField.font = UIFont(name: "Avenir Next", size: 14.0)
         searchField.placeholder = "Enter search term"
